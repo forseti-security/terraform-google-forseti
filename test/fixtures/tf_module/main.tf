a14bd4ec64f63153fd6110ff1a5eaaf5e2d69794
efouarge <eric.fouarge@gmail.com>
2018-11-05 09:21:25 -0600

    initial inspec testing and test-kitchen implementation

---
+++ test/fixtures/tf_module/main.tf
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google" {
+  credentials = "${file(var.credentials_path)}"
+}
+
+module "forseti-install-simple" {
+  source              = "../../../"
+  project_id          = "${var.project_id}"
+  gsuite_admin_email  = "${var.gsuite_admin_email}"
+  org_id              = "${var.org_id}"
+  server_type         = "n1-standard-4"
+  vpc_host_subnetwork = "${var.vpc_host_subnetwork}"
+  enable_cai_bucket   = "${var.enable_cai_bucket}"
+}
