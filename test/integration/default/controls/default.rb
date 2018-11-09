1c2b62c99ecb34604e03a3a29fa08ff43b7e3961
efouarge <eric.fouarge@gmail.com>
2018-11-09 00:59:12 -0600

    file structure cleanup, adding the Google header to files

--- test/integration/default/controls/default.rb
+++
@@ -1,8 +0,0 @@
-
-control "instance" do
-  describe google_compute_instance(project: 'rltech-173716', name: 'forseti-client-vm') do
-    it { should exist }
-    its('name') { should match 'forseti-client-vm' }
-  end
-end
-
