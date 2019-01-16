23277592ac1deb02f9e2219eaa70cb1f363860b8
Adrien Thebo <adrien@lagrange-automation.io>
2019-01-16 13:48:22 -0800

    Evaluate `var.enable_cai_bucket` in boolean context

--- server.tf
+++ server.tf
@@ -181,7 +181,7 @@ resource "google_storage_bucket_object" "forseti_server_config" {
 }
 
 resource "google_storage_bucket" "cai_export" {
-  count    = "${var.enable_cai_bucket == "true" ? 1 : 0}"
+  count    = "${var.enable_cai_bucket ? 1 : 0}"
   name     = "${local.storage_cai_bucket_name}"
   location = "${var.bucket_cai_location}"
   project  = "${var.project_id}"
