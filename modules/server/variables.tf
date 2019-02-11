46b0529096260d091b9316442db0501ce3e3ce10
hshin-g <shinhannah@google.com>
2019-02-11 13:45:51 -0800

    Parameterize values

--- modules/server/variables.tf
+++ modules/server/variables.tf
@@ -55,6 +55,458 @@ variable "forseti_run_frequency" {
   default     = "* */2 * * *"
 }
 
+#--------------------------#
+# Forseti config inventory #
+#--------------------------#
+variable "admin_max_calls" {
+  description = "Maximum calls that can be made to Admin API"
+  default     = "14"
+}
+
+variable "admin_period" {
+  description = "The period of max calls for the Admin API (in seconds)"
+  default     = "1.0"
+}
+
+variable "admin_disable_polling" {
+  description = "Whether to disable polling for Admin API"
+  default     = "False"
+}
+
+variable "appengine_max_calls" {
+  description = "Maximum calls that can be made to App Engine API"
+  default     = "18"
+}
+
+variable "appengine_period" {
+  description = "The period of max calls for the App Engine API (in seconds)"
+  default     = "1.0"
+}
+
+variable "appengine_disable_polling" {
+  description = "Whether to disable polling for App Engine API"
+  default     = "False"
+}
+
+variable "bigquery_max_calls" {
+  description = "Maximum calls that can be made to Big Query API"
+  default     = "160"
+}
+
+variable "bigquery_period" {
+  description = "The period of max calls for the Big Query API (in seconds)"
+  default     = "1.0"
+}
+
+variable "bigquery_disable_polling" {
+  description = "Whether to disable polling for Big Query API"
+  default     = "False"
+}
+
+variable "cloudasset_max_calls" {
+  description = "Maximum calls that can be made to Cloud Asset API"
+  default     = "1"
+}
+
+variable "cloudasset_period" {
+  description = "The period of max calls for the Cloud Asset API (in seconds)"
+  default     = "1.0"
+}
+
+variable "cloudasset_disable_polling" {
+  description = "Whether to disable polling for Cloud Asset API"
+  default     = "False"
+}
+
+variable "cloudbilling_max_calls" {
+  description = "Maximum calls that can be made to Cloud Billing API"
+  default     = "5"
+}
+
+variable "cloudbilling_period" {
+  description = "The period of max calls for the Cloud Billing API (in seconds)"
+  default     = "1.2"
+}
+
+variable "cloudbilling_disable_polling" {
+  description = "Whether to disable polling for Cloud Billing API"
+  default     = "False"
+}
+
+variable "compute_max_calls" {
+  description = "Maximum calls that can be made to Compute API"
+  default     = "18"
+}
+
+variable "compute_period" {
+  description = "The period of max calls for the Compute API (in seconds)"
+  default     = "1.0"
+}
+
+variable "compute_disable_polling" {
+  description = "Whether to disable polling for Compute API"
+  default     = "False"
+}
+
+variable "container_max_calls" {
+  description = "Maximum calls that can be made to Container API"
+  default     = "9"
+}
+
+variable "container_period" {
+  description = "The period of max calls for the Container API (in seconds)"
+  default     = "1.0"
+}
+
+variable "container_disable_polling" {
+  description = "Whether to disable polling for Container API"
+  default     = "False"
+}
+
+variable "crm_max_calls" {
+  description = "Maximum calls that can be made to CRN API"
+  default     = "4"
+}
+
+variable "crm_period" {
+  description = "The period of max calls for the CRM  API (in seconds)"
+  default     = "1.2"
+}
+
+variable "crm_disable_polling" {
+  description = "Whether to disable polling for CRM API"
+  default     = "False"
+}
+
+variable "iam_max_calls" {
+  description = "Maximum calls that can be made to IAM API"
+  default     = "90"
+}
+
+variable "iam_period" {
+  description = "The period of max calls for the IAM API (in seconds)"
+  default     = "1.0"
+}
+
+variable "iam_disable_polling" {
+  description = "Whether to disable polling for IAM API"
+  default     = "False"
+}
+variable "logging_max_calls" {
+  description = "Maximum calls that can be made to Logging API"
+  default     = "9"
+}
+
+variable "logging_period" {
+  description = "The period of max calls for the Logging API (in seconds)"
+  default     = "1.0"
+}
+
+variable "logging_disable_polling" {
+  description = "Whether to disable polling for Logging API"
+  default     = "False"
+}
+variable "securitycenter_max_calls" {
+  description = "Maximum calls that can be made to Security Center API"
+  default     = "1"
+}
+
+variable "securitycenter_period" {
+  description = "The period of max calls for the Security Center API (in seconds)"
+  default     = "1.1"
+}
+
+variable "securitycenter_disable_polling" {
+  description = "Whether to disable polling for Security Center API"
+  default     = "False"
+}
+
+variable "servicemanagement_max_calls" {
+  description = "Maximum calls that can be made to Service Management API"
+  default     = "2"
+}
+
+variable "servicemanagement_period" {
+  description = "The period of max calls for the Service Management API (in seconds)"
+  default     = "1.1"
+}
+
+variable "servicemanagement_disable_polling" {
+  description = "Whether to disable polling for Service Management API"
+  default     = "False"
+}
+
+variable "sqladmin_max_calls" {
+  description = "Maximum calls that can be made to SQL Admin API"
+  default     = "1"
+}
+
+variable "sqladmin_period" {
+  description = "The period of max calls for the SQL Admin API (in seconds)"
+  default     = "1.1"
+}
+
+variable "sqladmin_disable_polling" {
+  description = "Whether to disable polling for SQL Admin API"
+  default     = "False"
+}
+
+variable "storage_disable_polling" {
+  description = "Whether to disable polling for Storage API"
+  default     = "False"
+}
+
+variable "cai_api_timeout" {
+  description = "Timeout in seconds to wait for the exportAssets API to return success."
+  default     = "3600"
+}
+
+variable "cai_retention_days" {
+  description = "Number of days to retain inventory data."
+  default     = "-1"
+}
+
+#------------------------#
+# Forseti config scanner #
+#------------------------#
+variable "audit_logging_enabled" {
+  description = "Audit Logging scanner enabled."
+  default     = "false"
+}
+
+variable "biquery_enabled" {
+  description = "Big Query scanner enabled."
+  default     = "true"
+}
+
+variable "blacklist_enabled" {
+  description = "Audit Logging scanner enabled."
+  default     = "true"
+}
+
+variable "bucket_acl_enabled" {
+  description = "Bucket ACL scanner enabled."
+  default     = "true"
+}
+
+variable "cloudsql_acl_enabled" {
+  description = "Cloud SQL scanner enabled."
+  default     = "true"
+}
+
+variable "enabled_apis_enabled" {
+  description = "Enabled APIs scanner enabled."
+  default     = "false"
+}
+
+variable "firewall_rule_enabled" {
+  description = "Firewall rule scanner enabled."
+  default     = "true"
+}
+
+variable "forwarding_rule_enabled" {
+  description = "Forwarding rule scanner enabled."
+  default     = "false"
+}
+
+variable "group_enabled" {
+  description = "Group scanner enabled."
+  default     = "true"
+}
+
+variable "iam_policy_enabled" {
+  description = "IAM Policy scanner enabled."
+  default     = "true"
+}
+
+variable "iap_enabled" {
+  description = "IAP scanner enabled."
+  default     = "true"
+}
+
+variable "instance_network_interface_enabled" {
+  description = "Instance network interface scanner enabled."
+  default     = "false"
+}
+
+variable "ke_scanner_enabled" {
+  description = "KE scanner enabled."
+  default     = "false"
+}
+
+variable "ke_version_scanner_enabled" {
+  description = "KE version scanner enabled."
+  default     = "true"
+}
+
+variable "kms_scanner_enabled" {
+  description = "KMS scanner enabled."
+  default     = "true"
+}
+
+variable "lien_enabled" {
+  description = "Lien scanner enabled."
+  default     = "true"
+}
+
+variable "location_enabled" {
+  description = "Location scanner enabled."
+  default     = "true"
+}
+
+variable "log_sink_enabled" {
+  description = "Log sink scanner enabled."
+  default     = "true"
+}
+
+variable "resource_enabled" {
+  description = "Resource scanner enabled."
+  default     = "true"
+}
+
+variable "service_account_key_enabled" {
+  description = "Service account key scanner enabled."
+  default     = "true"
+}
+
+#-------------------------#
+# Forseti config notifier #
+#-------------------------#
+variable "iam_policy_violations_should_notify" {
+  description = "Notify for IAM Policy violations"
+  default     = "true"
+}
+
+variable "iam_policy_violations_slack_webhook" {
+  description = "Slack webhook for IAM Policy violations"
+  default     = ""
+}
+
+variable "audit_logging_violations_should_notify" {
+  description = "Notify for Audit logging violations"
+  default     = "true"
+}
+
+variable "blacklist_violations_should_notify" {
+  description = "Notify for Blacklist violations"
+  default     = "true"
+}
+
+variable "bigquery_acl_violations_should_notify" {
+  description = "Notify for BigQuery ACL violations"
+  default     = "true"
+}
+
+variable "buckets_acl_violations_should_notify" {
+  description = "Notify for Buckets ACL violations"
+  default     = "true"
+}
+
+variable "cloudsql_acl_violations_should_notify" {
+  description = "Notify for CloudSQL ACL violations"
+  default     = "true"
+}
+
+variable "enabled_apis_violations_should_notify" {
+  description = "Notify for enabled APIs violations"
+  default     = "true"
+}
+
+variable "firewall_rule_violations_should_notify" {
+  description = "Notify for Firewall rule violations"
+  default     = "true"
+}
+
+variable "forwarding_rule_violations_should_notify" {
+  description = "Notify for forwarding rule violations"
+  default     = "true"
+}
+
+variable "ke_version_violations_should_notify" {
+  description = "Notify for KE version violations"
+  default     = "true"
+}
+
+variable "ke_violations_should_notify" {
+  description = "Notify for KE violations"
+  default     = "true"
+}
+
+variable "kms_violations_should_notify" {
+  description = "Notify for KMS violations"
+  default     = "true"
+}
+
+variable "kms_violations_slack_webhook" {
+  description = "Slack webhook for KMS violations"
+  default     = ""
+}
+
+variable "groups_violations_should_notify" {
+  description = "Notify for Groups violations"
+  default     = "true"
+}
+
+variable "instance_network_interface_violations_should_notify" {
+  description = "Notify for instance network interface violations"
+  default     = "true"
+}
+
+variable "iap_violations_should_notify" {
+  description = "Notify for IAP violations"
+  default     = "true"
+}
+
+variable "lien_violations_should_notify" {
+  description = "Notify for lien violations"
+  default     = "true"
+}
+
+variable "location_violations_should_notify" {
+  description = "Notify for location violations"
+  default     = "true"
+}
+
+variable "log_sink_violations_should_notify" {
+  description = "Notify for log sink violations"
+  default     = "true"
+}
+
+variable "resource_violations_should_notify" {
+  description = "Notify for resource violations"
+  default     = "true"
+}
+
+variable "service_account_key_violations_should_notify" {
+  description = "Notify for service account key violations"
+  default     = "true"
+}
+
+variable "external_project_access_violations_should_notify" {
+  description = "Notify for External Project Access violations"
+  default     = "true"
+}
+
+variable "cscc_violations_enabled" {
+  description = "Notify for CSCC violations"
+  default     = "false"
+}
+
+variable "cscc_source_id" {
+  description = "Source ID for CSCC Beta API"
+  default     = ""
+}
+
+variable "inventory_gcs_summary_enabled" {
+  description = "GCS summary for inventory enabled"
+  default     = "true"
+}
+
+variable "inventory_email_summary_enabled" {
+  description = "Email summary for inventory enabled"
+  default     = "true"
+}
+
 #----------------#
 # Forseti server #
 #----------------#
