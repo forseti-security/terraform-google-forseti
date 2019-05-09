1796b197da4549d0032487c77b10db9c7776b434
hshin-g <43896566+hshin-g@users.noreply.github.com>
2019-05-09 16:41:59 -0700

    Swap role_rules to be alphabetical in list

--- modules/rules/main.tf
+++ modules/rules/main.tf
@@ -38,8 +38,8 @@ locals {
     "rules/log_sink_rules.yaml",
     "rules/resource_rules.yaml",
     "rules/retention_rules.yaml",
-    "rules/service_account_key_rules.yaml",
     "rules/role_rules.yaml",
+    "rules/service_account_key_rules.yaml",
   ]
 }
 
