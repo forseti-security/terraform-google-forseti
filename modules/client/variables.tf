72e6ee46b365515c815ec64ecdadbcd500e0ea37
hshin-g <shinhannah@google.com>
2019-05-13 09:11:05 -0700

    Change forseti_version to 2.15.0

--- modules/client/variables.tf
+++ modules/client/variables.tf
@@ -23,7 +23,7 @@ variable "project_id" {
 
 variable "forseti_version" {
   description = "The version of Forseti to install"
-  default     = "v2.14.1"
+  default     = "v2.15.0"
 }
 
 variable "forseti_repo_url" {
