c4b095ebb56169350c37fbf82fbca8b8f5950769
Olivier Cervello <ocervello@google.com>
2019-05-16 14:48:15 -0500

    Add bastion host in Shared VPC + remove public IP output

--- test/fixtures/shared_vpc/outputs.tf
+++ test/fixtures/shared_vpc/outputs.tf
@@ -29,11 +29,6 @@ output "forseti-server-vm-ip" {
   value       = "${module.forseti-shared-vpc.forseti-server-vm-ip}"
 }
 
-output "forseti-server-vm-public-ip" {
-  description = "Forseti Server VM public IP address"
-  value       = "${module.forseti-shared-vpc.forseti-server-vm-public-ip}"
-}
-
 output "forseti-server-vm-name" {
   description = "Forseti Server VM name"
   value       = "${module.forseti-shared-vpc.forseti-server-vm-name}"
@@ -42,11 +37,6 @@ output "forseti-server-vm-name" {
 output "forseti-client-vm-ip" {
   description = "Forseti Client VM private IP address"
   value       = "${module.forseti-shared-vpc.forseti-client-vm-ip}"
-}
-
-output "forseti-client-vm-public-ip" {
-  description = "Forseti Client VM public IP address"
-  value       = "${module.forseti-shared-vpc.forseti-client-vm-public-ip}"
 }
 
 output "forseti-client-vm-name" {
