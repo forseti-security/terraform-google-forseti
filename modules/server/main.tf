e3a27025dc0b9344e7d4c4bd545fa6056d43d30e
Ingwarr <sorceress1979@gmail.com>
2019-07-11 21:28:13 +0300

    Update modules/server/main.tf
    Co-Authored-By: Aaron Lane <aarondrewlane@gmail.com>

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -100,8 +100,8 @@ locals {
 # Input validation #
 #------------------#
 resource "null_resource" "missing_emails" {
-  count = "${local.missing_emails}"
-  #  "ERROR : `sendgrid_api_key` is set but `forseti_email_sender` or `forseti_email_recipient` are not. Please set those variables to enable email notifications." = true
+  count = local.missing_emails
+#  "ERROR : `sendgrid_api_key` is set but `forseti_email_sender` or `forseti_email_recipient` are not. Please set those variables to enable email notifications." = true
   provisioner "local-exec" {
     command     = "false"
     interpreter = ["bash", "-c"]
@@ -529,4 +529,3 @@ resource "null_resource" "services-dependency" {
     services = jsonencode(var.services)
   }
 }
-
