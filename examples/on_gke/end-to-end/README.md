1d00a0e714f0180eb8113cd4d800c71e78a971b4
Ken Evensen <kdevensen@google.com>
2019-07-16 13:37:46 -0400

    Adding in e2e example

---
+++ examples/on_gke/end-to-end/README.md
@@ -0,0 +1,38 @@
+# Forseti on GKE - End-to-End
+Follow this example you desire to deploy Forseti on GKE but are starting from an empty GCP project.  In otherwords, Forseti has not been yet been deployed.
+
+This example deploys the following:
+1. A new VPC
+2. Forseti infrstructure 
+3. A new GKE cluster - terraform-google-modules/kubernetes-engine/google
+4. Forseti on GKE - forseti-on-gke
+
+[^]: (autogen_docs_start)
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| credentials\_path | Path to service account json | string | n/a | yes |
+| forseti\_client\_service\_account | Forseti Client service account | string | n/a | yes |
+| forseti\_client\_vm\_ip | Forseti Client VM private IP address | string | n/a | yes |
+| forseti\_cloudsql\_connection\_name | The connection string to the CloudSQL instance | string | n/a | yes |
+| forseti\_server\_service\_account | Forseti Server service account | string | n/a | yes |
+| forseti\_server\_storage\_bucket | Forseti Server storage bucket | string | n/a | yes |
+| gke\_cluster\_name | The name of the GKE Cluster | string | `"forseti-cluster"` | no |
+| gke\_node\_ip\_range | The IP range for the GKE nodes. | string | `"10.1.0.0/20"` | no |
+| gke\_pod\_ip\_range | The IP range of the Kubernetes pods | string | `"10.2.0.0/20"` | no |
+| gke\_service\_account | The service account to run nodes as if not overridden in node_pools. The default value will cause a cluster-specific service account to be created. | string | `"create"` | no |
+| gke\_service\_ip\_range | The IP range of the Kubernetes services. | string | `"10.3.0.0/20"` | no |
+| helm\_repository\_url | The Helm repository containing the 'forseti-security' Helm charts | string | `"https://kubernetes-charts-incubator.storage.googleapis.com"` | no |
+| k8s\_forseti\_namespace | The Kubernetes namespace in which to deploy Forseti. | string | `"default"` | no |
+| k8s\_forseti\_orchestrator\_image | The container image for the Forseti orchestrator | string | `"gcr.io/forseti-containers/forseti:latest"` | no |
+| k8s\_forseti\_server\_image | The container image for the Forseti server | string | `"gcr.io/forseti-containers/forseti:latest"` | no |
+| k8s\_tiller\_sa\_name | The Kubernetes Service Account used by Tiller | string | `"tiller"` | no |
+| network\_name | The name of the VPC being created | string | `"gke-network"` | no |
+| project\_id | The ID of an existing Google project where Forseti will be installed | string | n/a | yes |
+| region | Region where forseti subnetwork will be deployed | string | n/a | yes |
+| sub\_network\_name | The names of the subnet being created | string | `"gke-sub-network"` | no |
+| zones | The zones to host the cluster in (optional if regional cluster / required if zonal) | list | `<list>` | no |
+
+[^]: (autogen_docs_end)
---
+++ examples/on_gke/end-to-end/main.tf
@@ -0,0 +1,172 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//*****************************************
+//  Setup Google providers
+//*****************************************
+
+provider "google" {
+  version = "~> 2.7.0"
+  credentials = "${file(var.credentials_path)}"
+}
+
+provider "google-beta" {
+  version = "~> 2.7.0"
+  credentials = "${file(var.credentials_path)}"
+}
+
+//*****************************************
+//  Setup the Kubernetes Provider
+//*****************************************
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  alias                   = "forseti"
+  load_config_file        = false
+
+  host                    = "https://${module.gke.endpoint}"
+  token                   = "${data.google_client_config.default.access_token}"
+  cluster_ca_certificate  = "${base64decode(module.gke.ca_certificate)}"
+}
+
+//*****************************************
+//  Setup Helm Provider
+//*****************************************
+
+provider "helm" {
+  alias           = "forseti"
+  service_account = "${var.k8s_tiller_sa_name}"
+  namespace       = "${var.k8s_forseti_namespace}-${module.forseti.suffix}"
+  kubernetes {
+    load_config_file = false
+    host = "https://${module.gke.endpoint}"
+    token = "${data.google_client_config.default.access_token}"
+    cluster_ca_certificate = "${base64decode(module.gke.ca_certificate)}"
+  }
+  debug = true
+  automount_service_account_token = true
+  install_tiller = true
+}
+
+resource "google_project_service" "gcr" {
+  project            = "${var.project_id}"
+  service            = "containerregistry.googleapis.com"
+  disable_on_destroy = "false"
+}
+
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "0.8.0"
+
+  project_id   = "${var.project_id}"
+  network_name = "${var.network_name}"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+      {
+          subnet_name           = "${var.sub_network_name}"
+          subnet_ip             = "${var.gke_node_ip_range}"
+          subnet_region         = "${var.region}"
+      },
+  ]
+
+  secondary_ranges = {
+    "${var.sub_network_name}" = [
+      {
+        range_name    = "gke-pod-ip-range"
+        ip_cidr_range = "${var.gke_pod_ip_range}"
+      },
+      {
+        range_name    = "gke-service-ip-range"
+        ip_cidr_range = "${var.gke_service_ip_range}"
+      },
+    ]
+  }
+}
+
+module "forseti" {
+	source  = "terraform-google-modules/forseti/google"
+	version = "~> 2.3.0"
+
+	gsuite_admin_email = "${var.gsuite_admin_email}"
+	domain             = "${var.domain}"
+	project_id         = "${var.project_id}"
+	org_id             = "${var.org_id}"
+  network            = "${module.vpc.network_name}"
+  client_private     = true
+  server_private     = true
+}
+
+module "gke" {
+  source                  = "terraform-google-modules/kubernetes-engine/google"
+  version                 = "3.0.0"
+  project_id              = "${var.project_id}"
+  name                    = "${var.gke_cluster_name}"
+  regional                = false
+  region                  = "${var.region}"
+  zones                   = "${var.zones}"
+  network                 = "${module.vpc.network_name}"
+  subnetwork              = "${module.vpc.subnets_names[0]}"
+  ip_range_pods           = "gke-pod-ip-range"
+  ip_range_services       = "gke-service-ip-range"
+  service_account         = "${var.gke_service_account}"
+  network_policy          = true
+
+  node_pools = [
+    {
+        name               = "default-node-pool"
+        machine_type       = "n1-standard-2"
+        min_count          = 1
+        max_count          = 1
+        disk_size_gb       = 100
+        disk_type          = "pd-standard"
+        image_type         = "COS"
+        auto_repair        = true
+        auto_upgrade       = true
+        preemptible        = false
+        initial_node_count = 1
+    },
+  ]
+
+  node_pools_oauth_scopes {
+      all = []
+
+      default-node-pool = [
+          "https://www.googleapis.com/auth/cloud-platform",
+      ]
+  }
+}
+
+//*****************************************
+//  Deploy Forseti on GKE
+//*****************************************
+
+module "forseti-on-gke" {
+  providers = {
+    kubernetes  = "kubernetes.forseti"
+    helm        = "helm.forseti"
+  }
+  source                                = "../../../modules/on_gke"
+  forseti_client_service_account        = "${module.forseti.forseti-client-service-account}"
+  forseti_client_vm_ip                  = "${module.forseti.forseti-client-vm-ip}"
+  forseti_cloudsql_connection_name      = "${module.forseti.forseti-cloudsql-connection-name}"
+  forseti_server_service_account        = "${module.forseti.forseti-server-service-account}"
+  forseti_server_bucket                 = "${module.forseti.forseti-server-storage-bucket}"
+  gke_service_account                   = "${module.gke.service_account}"
+  project_id                            = "${var.project_id}"
+  network_policy                        = "${module.gke.network_policy_enabled}"
+}
