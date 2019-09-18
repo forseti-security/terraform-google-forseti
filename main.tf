8d607437e345d8310ce3cd48f4eea21353d6e576
Ken Evensen <kdevensen@google.com>
2019-09-18 09:06:24 -0700

    Server config submodule - 4

--- main.tf
+++ main.tf
@@ -112,40 +112,94 @@ module "client" {
 module "server" {
   source = "./modules/server"
 
-  project_id                                          = var.project_id
-  gsuite_admin_email                                  = var.gsuite_admin_email
-  forseti_version                                     = var.forseti_version
-  forseti_repo_url                                    = var.forseti_repo_url
+  project_id                   = var.project_id
+  forseti_version              = var.forseti_version
+  forseti_repo_url             = var.forseti_repo_url
+  forseti_home                 = var.forseti_home
+  forseti_run_frequency        = var.forseti_run_frequency
+  client_service_account_email = module.client.forseti-client-service-account
+  server_type                  = var.server_type
+  server_region                = var.server_region
+  server_boot_image            = var.server_boot_image
+  server_boot_disk_size        = var.server_boot_disk_size
+  server_boot_disk_type        = var.server_boot_disk_type
+  server_tags                  = var.server_tags
+  server_access_config         = var.server_access_config
+  server_private               = var.server_private
+  server_iam_module            = module.server_iam
+  cloudsql_module              = module.cloudsql
+  server_gcs_module            = module.server_gcs
+  server_rules_module          = module.server_rules
+  server_config_module         = module.server_config
+  cloudsql_proxy_arch          = var.cloudsql_proxy_arch
+  network                      = var.network
+  subnetwork                   = var.subnetwork
+  network_project              = var.network_project
+  server_grpc_allow_ranges     = var.server_grpc_allow_ranges
+  server_ssh_allow_ranges      = var.server_ssh_allow_ranges
+  server_instance_metadata     = var.server_instance_metadata
+  suffix                       = local.random_hash
+
+  policy_library_home                    = var.policy_library_home
+  policy_library_repository_url          = var.policy_library_repository_url
+  policy_library_sync_enabled            = var.policy_library_sync_enabled
+  policy_library_sync_gcs_directory_name = var.policy_library_sync_gcs_directory_name
+  policy_library_sync_git_sync_tag       = var.policy_library_sync_git_sync_tag
+  policy_library_sync_ssh_known_hosts    = var.policy_library_sync_ssh_known_hosts
+
+  services = google_project_service.main.*.service
+}
+
+module "cloudsql" {
+  source             = "./modules/cloudsql"
+  cloudsql_disk_size = var.cloudsql_disk_size
+  cloudsql_private   = var.cloudsql_private
+  cloudsql_region    = var.cloudsql_region
+  cloudsql_type      = var.cloudsql_type
+  network_project    = var.network_project
+  project_id         = var.project_id
+  services           = google_project_service.main.*.service
+  suffix             = local.random_hash
+}
+
+module "server_iam" {
+  source                  = "./modules/server_iam"
+  cscc_violations_enabled = var.cscc_violations_enabled
+  enable_write            = var.enable_write
+  project_id              = var.project_id
+  org_id                  = var.org_id
+  suffix                  = local.random_hash
+}
+
+module "server_gcs" {
+  source                   = "./modules/server_gcs"
+  project_id               = var.project_id
+  bucket_cai_location      = var.bucket_cai_location
+  bucket_cai_lifecycle_age = var.bucket_cai_lifecycle_age
+  enable_cai_bucket        = var.enable_cai_bucket
+  storage_bucket_location  = var.storage_bucket_location
+  services                 = google_project_service.main.*.service
+  suffix                   = local.random_hash
+}
+
+module "server_rules" {
+  source            = "./modules/rules"
+  server_gcs_module = module.server_gcs
+  org_id            = var.org_id
+  domain            = var.domain
+}
+
+module "server_config" {
+  source                                              = "./modules/server_config"
+  composite_root_resources                            = var.composite_root_resources
+  server_gcs_module                                   = module.server_gcs
   forseti_email_recipient                             = var.forseti_email_recipient
   forseti_email_sender                                = var.forseti_email_sender
-  forseti_home                                        = var.forseti_home
-  forseti_run_frequency                               = var.forseti_run_frequency
-  client_service_account_email                        = module.client.forseti-client-service-account
-  server_type                                         = var.server_type
-  server_region                                       = var.server_region
-  server_boot_image                                   = var.server_boot_image
-  server_boot_disk_size                               = var.server_boot_disk_size
-  server_boot_disk_type                               = var.server_boot_disk_type
-  server_tags                                         = var.server_tags
-  server_access_config                                = var.server_access_config
-  server_private                                      = var.server_private
-  server_iam_module                                   = module.server_iam
-  cloudsql_module                                     = module.cloudsql
-  server_gcs_module                                   = module.server_gcs
-  server_rules_module                                 = module.server_rules
-  cloudsql_proxy_arch                                 = var.cloudsql_proxy_arch
-  network                                             = var.network
-  subnetwork                                          = var.subnetwork
-  network_project                                     = var.network_project
-  server_grpc_allow_ranges                            = var.server_grpc_allow_ranges
-  server_ssh_allow_ranges                             = var.server_ssh_allow_ranges
-  domain                                              = var.domain
+  sendgrid_api_key                                    = var.sendgrid_api_key
   org_id                                              = var.org_id
   folder_id                                           = var.folder_id
-  composite_root_resources                            = var.composite_root_resources
-  sendgrid_api_key                                    = var.sendgrid_api_key
-  suffix                                              = local.random_hash
-  server_instance_metadata                            = var.server_instance_metadata
+  domain                                              = var.domain
+  gsuite_admin_email                                  = var.gsuite_admin_email
   storage_disable_polling                             = var.storage_disable_polling
   sqladmin_period                                     = var.sqladmin_period
   sqladmin_max_calls                                  = var.sqladmin_max_calls
@@ -239,57 +293,10 @@ module "server" {
   violations_slack_webhook                            = var.violations_slack_webhook
   cscc_violations_enabled                             = var.cscc_violations_enabled
   cscc_source_id                                      = var.cscc_source_id
-  policy_library_home                                 = var.policy_library_home
-  policy_library_repository_url                       = var.policy_library_repository_url
-  policy_library_sync_enabled                         = var.policy_library_sync_enabled
-  policy_library_sync_gcs_directory_name              = var.policy_library_sync_gcs_directory_name
-  policy_library_sync_git_sync_tag                    = var.policy_library_sync_git_sync_tag
-  policy_library_sync_ssh_known_hosts                 = var.policy_library_sync_ssh_known_hosts
 
   groups_settings_max_calls                = var.groups_settings_max_calls
   groups_settings_period                   = var.groups_settings_period
   groups_settings_disable_polling          = var.groups_settings_disable_polling
   groups_settings_enabled                  = var.groups_settings_enabled
   groups_settings_violations_should_notify = var.groups_settings_violations_should_notify
-
-  services = google_project_service.main.*.service
-}
-
-module "cloudsql" {
-  source             = "./modules/cloudsql"
-  cloudsql_disk_size = var.cloudsql_disk_size
-  cloudsql_private   = var.cloudsql_private
-  cloudsql_region    = var.cloudsql_region
-  cloudsql_type      = var.cloudsql_type
-  network_project    = var.network_project
-  project_id         = var.project_id
-  services           = google_project_service.main.*.service
-  suffix             = local.random_hash
-}
-
-module "server_iam" {
-  source                  = "./modules/server_iam"
-  cscc_violations_enabled = var.cscc_violations_enabled
-  enable_write            = var.enable_write
-  project_id              = var.project_id
-  org_id                  = var.org_id
-  suffix                  = local.random_hash
-}
-
-module "server_gcs" {
-  source                   = "./modules/server_gcs"
-  project_id               = var.project_id
-  bucket_cai_location      = var.bucket_cai_location
-  bucket_cai_lifecycle_age = var.bucket_cai_lifecycle_age
-  enable_cai_bucket        = var.enable_cai_bucket
-  storage_bucket_location  = var.storage_bucket_location
-  services                 = google_project_service.main.*.service
-  suffix                   = local.random_hash
-}
-
-module "server_rules" {
-  source            = "./modules/rules"
-  server_gcs_module = module.server_gcs
-  org_id            = var.org_id
-  domain            = var.domain
 }
