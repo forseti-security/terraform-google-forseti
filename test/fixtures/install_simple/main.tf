3ebf3c58de28cbabbc7bc928b5f6b3d86f4fa31a
Va Ha <goofiva@gmail.com>
2019-12-01 13:22:41 -0800

    Initial commit of install_simple fixture.

---
+++ test/fixtures/install_simple/main.tf
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "tls" {
+  version = "~> 2.0"
+}
+
+resource "tls_private_key" "main" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "gce-keypair-pk" {
+  content  = tls_private_key.main.private_key_pem
+  filename = "${path.module}/sshkey"
+}
+
+data "google_compute_zones" "main" {
+  project = var.project_id
+  region  = var.region
+  status  = "UP"
+}
+
+module "bastion" {
+  source = "../bastion"
+
+  network    = var.network
+  project_id = var.project_id
+  subnetwork = var.subnetwork
+  zone       = data.google_compute_zones.main.names[0]
+  key_suffix = "_install_simple"
+}
+
+module "forseti-install-simple" {
+  source = "../../../examples/install_simple"
+
+  gsuite_admin_email = var.gsuite_admin_email
+  project_id         = var.project_id
+  org_id             = var.org_id
+  domain             = var.domain
+  region             = var.region
+  network            = var.network
+  subnetwork         = var.subnetwork
+  forseti_version    = var.forseti_version
+
+  instance_metadata = {
+    sshKeys = "ubuntu:${tls_private_key.main.public_key_openssh}"
+  }
+}
+
+resource "google_compute_firewall" "forseti_bastion_to_vm" {
+
+  name    = "forseti-bastion-to-vm-ssh-${module.forseti-install-simple.suffix}"
+  project = var.project_id
+  network = var.network
+  target_service_accounts = [module.forseti-install-simple.forseti-server-service-account,
+  module.forseti-install-simple.forseti-client-service-account]
+
+  source_ranges = ["${module.bastion.host-private-ip}/32"]
+  direction     = "INGRESS"
+  priority      = "100"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+}
+
+resource "null_resource" "wait_for_server" {
+  triggers = {
+    always_run = uuid()
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/scripts/wait-for-forseti.sh"
+
+    connection {
+      type                = "ssh"
+      user                = "ubuntu"
+      host                = module.forseti-install-simple.forseti-server-vm-ip
+      private_key         = tls_private_key.main.private_key_pem
+      bastion_host        = module.bastion.host
+      bastion_port        = module.bastion.port
+      bastion_private_key = module.bastion.private_key
+      bastion_user        = module.bastion.user
+    }
+  }
+  depends_on = [
+    google_compute_firewall.forseti_bastion_to_vm
+  ]
+}
+
+resource "null_resource" "wait_for_client" {
+  triggers = {
+    always_run = uuid()
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/scripts/wait-for-forseti.sh"
+
+    connection {
+      type                = "ssh"
+      user                = "ubuntu"
+      host                = module.forseti-install-simple.forseti-client-vm-ip
+      private_key         = tls_private_key.main.private_key_pem
+      bastion_host        = module.bastion.host
+      bastion_port        = module.bastion.port
+      bastion_private_key = module.bastion.private_key
+      bastion_user        = module.bastion.user
+    }
+  }
+
+  depends_on = [
+    google_compute_firewall.forseti_bastion_to_vm
+  ]
+}
---
+++ test/integration/install_simple/controls/forseti.rb
@@ -0,0 +1,224 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# encoding: utf-8
+
+org_id = attribute('org_id')
+project_id = attribute('project_id')
+network_project = attribute('network_project').empty? ? project_id : attribute('network_project')
+suffix = attribute('suffix')
+forseti_client_vm_name = attribute('forseti-client-vm-name')
+forseti_server_vm_name = attribute('forseti-server-vm-name')
+forseti_client_storage_bucket = attribute('forseti-client-storage-bucket')
+forseti_server_storage_bucket = attribute('forseti-server-storage-bucket')
+forseti_client_service_account = attribute('forseti-client-service-account')
+forseti_server_service_account = attribute('forseti-server-service-account')
+
+control 'forseti' do
+  title "Forseti GCP resources"
+
+  describe google_compute_instance(
+    project: project_id,
+    zone: 'us-central1-c',
+    name: forseti_client_vm_name
+  ) do
+    it { should exist }
+    its('machine_size') { should eq 'n1-standard-2' }
+  end
+
+  describe google_compute_instance(
+    project: project_id,
+    zone: 'us-central1-c',
+    name: forseti_server_vm_name
+  ) do
+    it { should exist }
+    its('machine_size') { should eq 'n1-standard-8' }
+  end
+
+  describe google_sql_database_instances(project: project_id) do
+    its('instance_names') { should include(/forseti-server-db-*/) }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/storage.objectViewer") do
+    it { should exist }
+    its('members') { should include "serviceAccount:#{forseti_server_service_account}" }
+  end
+  describe google_project_iam_binding(project: project_id, role: "roles/storage.objectCreator") do
+    it { should exist }
+    its('members') { should include "serviceAccount:#{forseti_server_service_account}" }
+  end
+  describe google_project_iam_binding(project: project_id, role: "roles/cloudsql.client") do
+    it { should exist }
+    its('members') { should include "serviceAccount:#{forseti_server_service_account}" }
+  end
+  describe google_project_iam_binding(project: project_id, role: "roles/logging.logWriter") do
+    it { should exist }
+    its('members') { should include "serviceAccount:#{forseti_server_service_account}" }
+  end
+  describe google_project_iam_binding(project: project_id, role: "roles/iam.serviceAccountTokenCreator") do
+    it { should exist }
+    its('members') { should include "serviceAccount:#{forseti_server_service_account}" }
+  end
+
+  describe google_storage_buckets(project: project_id) do
+    its('bucket_names') { should include forseti_server_storage_bucket }
+    its('bucket_names') { should include forseti_client_storage_bucket }
+    its('bucket_names') { should include(/forseti-cai-export/) }
+  end
+
+  describe google_storage_bucket_objects(bucket: forseti_server_storage_bucket) do
+
+    # Enumerate the files that we expect to be present. This fixture ensures that we
+    # don't silently drop a rules file.
+    let(:expected_files) do
+      %w[
+        rules/audit_logging_rules.yaml
+        rules/bigquery_rules.yaml
+        rules/blacklist_rules.yaml
+        rules/bucket_rules.yaml
+        rules/cloudsql_rules.yaml
+        rules/enabled_apis_rules.yaml
+        rules/external_project_access_rules.yaml
+        rules/firewall_rules.yaml
+        rules/forwarding_rules.yaml
+        rules/group_rules.yaml
+        rules/groups_settings_rules.yaml
+        rules/iam_rules.yaml
+        rules/iap_rules.yaml
+        rules/instance_network_interface_rules.yaml
+        rules/ke_rules.yaml
+        rules/ke_scanner_rules.yaml
+        rules/lien_rules.yaml
+        rules/location_rules.yaml
+        rules/log_sink_rules.yaml
+        rules/resource_rules.yaml
+        rules/retention_rules.yaml
+        rules/role_rules.yaml
+        rules/service_account_key_rules.yaml
+      ]
+    end
+
+    # Enumerate the files that are present in the rules directory  This fixture ensures
+    # that we don't miss an included rules file.
+    let(:present_files) do
+      template_dir = File.expand_path(
+        "../../../../modules/rules/templates/rules",
+        __dir__
+      )
+      Dir.glob("#{template_dir}/*.yaml").map {|file| "rules/#{File.basename(file)}" }
+    end
+
+    let(:files) { expected_files | present_files }
+
+    its('object_names') { should include(*files) }
+  end
+
+  describe google_service_account(name: "projects/#{project_id}/serviceAccounts/#{forseti_client_service_account}") do
+    its(:email) { should eq forseti_client_service_account }
+    its(:display_name) { should eq "Forseti Client Service Account" }
+  end
+
+  describe google_service_account(name: "projects/#{project_id}/serviceAccounts/#{forseti_server_service_account}") do
+    its(:email) { should eq forseti_server_service_account }
+    its(:display_name) { should eq "Forseti Server Service Account" }
+  end
+
+  describe google_compute_firewall(project: network_project, name: "forseti-server-ssh-iap-#{suffix}") do
+    its('source_ranges') { should eq ["35.235.240.0/20"] }
+    its('direction') { should eq 'INGRESS' }
+    its('allowed_ssh?') { should be true }
+    its('priority') { should eq 100 }
+  end
+
+  describe google_compute_firewall(project: network_project, name: "forseti-server-allow-grpc-#{suffix}") do
+    let(:allowed) { subject.allowed.map(&:item) }
+
+    its('source_ranges') { should eq ["10.128.0.0/9"] }
+    its('direction') { should eq 'INGRESS' }
+    its('priority') { should eq 100 }
+
+    it "allows gRPC traffic" do
+      expect(allowed).to contain_exactly({ip_protocol: "tcp", ports: ["50051", "50052"]})
+    end
+  end
+
+  describe google_compute_firewall(project: network_project, name: "forseti-server-deny-all-#{suffix}") do
+    let(:denied) { subject.denied.map(&:item) }
+
+    its('source_ranges') { should eq ["0.0.0.0/0"] }
+    its('direction') { should eq 'INGRESS' }
+    its('priority') { should eq 200 }
+
+    it "denies TCP, UDP, and ICMP" do
+      expect(denied).to contain_exactly(
+        {ip_protocol: "icmp"},
+        {ip_protocol: "tcp"},
+        {ip_protocol: "udp"}
+      )
+    end
+  end
+
+  describe google_compute_firewall(project: network_project, name: "forseti-client-ssh-iap-#{suffix}") do
+    its('source_ranges') { should eq ["35.235.240.0/20"] }
+    its('direction') { should eq 'INGRESS' }
+    its('allowed_ssh?') { should be true }
+    its('priority') { should eq 100 }
+  end
+
+  describe google_compute_firewall(project: network_project, name: "forseti-client-deny-all-#{suffix}") do
+    let(:denied) { subject.denied.map(&:item) }
+
+    its('source_ranges') { should eq ["0.0.0.0/0"] }
+    its('direction') { should eq 'INGRESS' }
+    its('priority') { should eq 200 }
+
+    it "denies TCP, UDP, and ICMP" do
+      expect(denied).to contain_exactly(
+        {ip_protocol: "icmp"},
+        {ip_protocol: "tcp"},
+        {ip_protocol: "udp"}
+      )
+    end
+  end
+end
+
+control 'forseti-org-iam' do
+  title "Validate organization roles of SA"
+  describe command("gcloud organizations get-iam-policy #{org_id} --filter='bindings.members:#{forseti_server_service_account}' --flatten='bindings[].members' --format='json(bindings.role)'") do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq '' }
+
+    let(:sa_roles) do
+      JSON.parse(subject.stdout).map { |a| a["bindings"]["role"] }
+    end
+
+    let(:expected_roles) do
+      [
+        "roles/appengine.appViewer",
+        "roles/bigquery.metadataViewer",
+        "roles/browser",
+        "roles/cloudasset.viewer",
+        "roles/cloudsql.viewer",
+        "roles/compute.networkViewer",
+        "roles/iam.securityReviewer",
+        "roles/orgpolicy.policyViewer",
+        "roles/servicemanagement.quotaViewer",
+        "roles/serviceusage.serviceUsageConsumer",
+      ]
+    end
+
+    it 'has all expected org roles' do
+      expect(sa_roles).to match_array(expected_roles)
+    end
+  end
+end
