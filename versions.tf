5288827c0b284f41c8390b8f66740bca45888b7e
Rehana Devani <rdevani@google.com>
2020-02-07 14:46:59 -0500

    Pinned helm provider version to 0.10.*

--- versions.tf
+++ versions.tf
@@ -19,6 +19,7 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     google   = "~> 2.11"
+    helm     = "~> 0.10"
     null     = "~> 2.0"
     template = "~> 2.0"
     random   = "~> 2.0"
