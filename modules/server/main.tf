5194707256118117eec8da82b97becd4dbd7bfe9
Rehana Devani <rdevani@google.com>
2020-03-03 12:46:12 -0500

    Removed forseti_env.sh script

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -32,9 +32,6 @@ locals {
   server_environment = file(
     "${path.module}/templates/scripts/forseti-server/forseti_environment.sh.tpl",
   )
-  server_env = file(
-    "${path.module}/templates/scripts/forseti-server/forseti_env.sh.tpl",
-  )
   server_initialize_services = file(
     "${path.module}/templates/scripts/forseti-server/initialize_forseti_services.sh.tpl",
   )
@@ -70,7 +67,6 @@ data "template_file" "forseti_server_startup_script" {
     cloudsql_proxy_arch                    = var.cloudsql_proxy_arch
     cloud_profiler_enabled                 = var.cloud_profiler_enabled
     forseti_conf_server_checksum           = base64sha256(var.server_config_module.forseti-server-config)
-    forseti_env                            = data.template_file.forseti_server_env.rendered
     forseti_environment                    = data.template_file.forseti_server_environment.rendered
     forseti_home                           = var.forseti_home
     forseti_repo_url                       = var.forseti_repo_url
@@ -98,21 +94,6 @@ data "template_file" "forseti_server_environment" {
     policy_library_repository_url    = var.policy_library_repository_url
     policy_library_sync_git_sync_tag = var.policy_library_sync_git_sync_tag
     storage_bucket_name              = var.server_gcs_module.forseti-server-storage-bucket
-  }
-}
-
-data "template_file" "forseti_server_env" {
-  template = local.server_env
-
-  vars = {
-    project_id             = var.project_id
-    cloudsql_db_name       = var.cloudsql_module.forseti-cloudsql-db-name
-    cloudsql_db_port       = var.cloudsql_module.forseti-cloudsql-db-port
-    cloudsql_region        = var.cloudsql_module.forseti-cloudsql-region
-    cloudsql_instance_name = var.cloudsql_module.forseti-cloudsql-instance-name
-    cloudsql_db_user       = var.cloudsql_module.forseti-cloudsql-user
-    cloudsql_db_password   = var.cloudsql_module.forseti-cloudsql-password
-    forseti_scripts        = var.forseti_scripts
   }
 }
 
