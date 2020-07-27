3763543347883bbdbe9065f448a0f5a7deccdf52
Gregg Kowalski <gkowalski@google.com>
2020-07-27 12:02:54 -0700

    Fix lint.

--- modules/cloudsql/main.tf
+++ modules/cloudsql/main.tf
@@ -46,15 +46,15 @@ resource "google_compute_global_address" "private_ip_address" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = "${data.google_compute_network.cloudsql_private_network.self_link}"
+  network       = data.google_compute_network.cloudsql_private_network.self_link
   depends_on    = ["google_project_service.service_networking"]
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
   count                   = var.cloudsql_private ? 1 : 0
-  network                 = "${data.google_compute_network.cloudsql_private_network.self_link}"
+  network                 = data.google_compute_network.cloudsql_private_network.self_link
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = ["${google_compute_global_address.private_ip_address[count.index].name}"]
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address[count.index].name]
 }
 
 #----------------------#
