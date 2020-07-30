656cce0b93aed6950c19342c76b37ef4ad99af47
Gregg Kowalski <10247435+gkowalski-google@users.noreply.github.com>
2020-07-29 20:06:51 -0700

    Forseti module release 5.1.4 for Forseti 2.24.3 (#599)
    * Standard changes for module v5.1.4 release. Fix forseti gcp tests. Fix lint and add GCB config for release int tests.
    
    * Update source for install_simple to point to relative path instead of Terraform repository.

--- modules/client/variables.tf
+++ modules/client/variables.tf
@@ -23,7 +23,7 @@ variable "project_id" {
 
 variable "forseti_version" {
   description = "The version of Forseti to install"
-  default     = "v2.24.2"
+  default     = "v2.24.3"
 }
 
 variable "forseti_repo_url" {
