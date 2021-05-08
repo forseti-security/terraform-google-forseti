1538a424fcc2b5ae0ade97e7029239085ff0dd83
Gregg Kowalski <gkowalski@google.com>
2021-05-07 18:03:03 -0700

    Update google cloud sdk to 335.0.0-0

--- modules/server/variables.tf
+++ modules/server/variables.tf
@@ -128,7 +128,7 @@ variable "mailjet_enabled" {
 
 variable "google_cloud_sdk_version" {
   description = "Version of the Google Cloud SDK to install"
-  default     = "289.0.0-0"
+  default     = "335.0.0-0"
   type        = string
 }
 
