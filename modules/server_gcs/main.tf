c0baf717d9f77c4a9c2041756eb5272b62cdfc41
CpBulletSponge <ollyrichards@gmail.com>
2021-06-23 14:17:31 +0100

    Update main.tf

--- modules/server_gcs/main.tf
+++ modules/server_gcs/main.tf
@@ -28,26 +28,26 @@ locals {
 #------------------------#
 
 resource "google_storage_bucket" "server_config" {
-  name               = local.server_bucket_name
-  location           = var.storage_bucket_location
-  project            = var.project_id
-  storage_class      = var.storage_bucket_class
-  force_destroy      = true
-  bucket_policy_only = true
-  labels             = var.gcs_labels
+  name                        = local.server_bucket_name
+  location                    = var.storage_bucket_location
+  project                     = var.project_id
+  storage_class               = var.storage_bucket_class
+  force_destroy               = true
+  uniform_bucket_level_access = true
+  labels                      = var.gcs_labels
 
   depends_on = [null_resource.services-dependency]
 }
 
 resource "google_storage_bucket" "cai_export" {
-  count              = var.enable_cai_bucket ? 1 : 0
-  name               = local.storage_cai_bucket_name
-  location           = var.bucket_cai_location
-  project            = var.project_id
-  storage_class      = var.storage_bucket_class
-  force_destroy      = true
-  bucket_policy_only = true
-  labels             = var.gcs_labels
+  count                       = var.enable_cai_bucket ? 1 : 0
+  name                        = local.storage_cai_bucket_name
+  location                    = var.bucket_cai_location
+  project                     = var.project_id
+  storage_class               = var.storage_bucket_class
+  force_destroy               = true
+  uniform_bucket_level_access = true
+  labels                      = var.gcs_labels
 
   lifecycle_rule {
     action {
